47e21928416d1b345d5a5798193ebdc0a4ea56db
Daniella Albuquerque dos Angelos <daniaangelos@gmail.com>
2018-12-13 10:08:36 -0200

    conexao com mongodb atlas estabelecida

--- novabot-api/index.js
+++ novabot-api/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 const path = require('path')
+const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
+
+var uri = "mongodb+srv://dani:12345@cluster0-hvnbg.mongodb.net/test?retryWrites=true";
+
+MongoClient.connect(uri, function(err, db) {
+  if(err) console.log(err.msg)
+  else {
+    db.close();
+  }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,6 +21,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/test', (req, res) => {res.send({"status":200, "Content-type":"application/json", "challenge":req.body.challenge});})
-  .post('/novaquote', (req, res) => res.send({"status": 200, "Content-type":"application/json", "body":req.body.text}))
-  .listen(PORT, () => console.log('Novabot API listening on ${ PORT }'))
+  .post('/novaquote', (req, res) => res.send(req.body))
+  .listen(PORT, () => console.log(`Novabot API listening on ${ PORT }`))
 
